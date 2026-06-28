4a48f08386725e30cdd50912bc86a6584aa449a6
Harsh-Kumar-Mishra <mishrakumarharsh987@gmail.com>
2026-06-28 21:13:14 +0530

    Added frontend endpoint

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ connectDB();
 // CORS configuration
 const allowedOrigins = [
   'http://localhost:3000',
-  'http://localhost:5173'
+  'http://localhost:5173',
+  'https://colledge-connect-assignment-frontend.onrender.com'
 ].filter(Boolean);
 
 app.use(cors({
